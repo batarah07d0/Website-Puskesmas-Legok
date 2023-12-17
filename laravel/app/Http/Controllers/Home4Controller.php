c1b3e79a2925552e2db43e3f643e38d76f98cc20
andreo_friendly <andreofriendly.t.m@gmail.com>
2023-12-17 21:03:19 +0700

    LatestNewest

--- laravel/app/Http/Controllers/Home4Controller.php
+++ laravel/app/Http/Controllers/Home4Controller.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Dokter;
 use App\Models\Staff;
 
 class Home4Controller extends Controller
@@ -13,14 +12,4 @@ class Home4Controller extends Controller
         $list = Staff::all();
         return view('tentangkami', compact('list'));
     }
-        public function index()
-        {
-                $list = Dokter::all();
-                $namas = Dokter::all();
-                $lists = Staff::all();
-                $namast = Staff::all();
-                $fotop = Prestasi::all();
-                $namap = Prestasi::all();
-                return view('tentangkami', compact('list', 'namas', 'fotop', 'namap', 'lists', 'namast'));
-        }
 }
